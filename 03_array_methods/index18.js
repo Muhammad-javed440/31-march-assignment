71ca3d6c1fe1b121b591938a9016ed6c3b10e144
Muhammad-javed440 <meherjaved440@gmail.com>
2024-05-02 05:54:51 +0600

    18 question completed

--- 03_array_methods/index18.js
+++ 03_array_methods/index18.js
@@ -1,8 +1,19 @@
 "use strict";
 // Q18. Develop a function called sumOfElements that calculates the sum of 
 // all elements in an array that are either even or odd, based on a parameter.
-// let array4 : number[] = [1,2,3,4,5,6,7,8,9,10];
-// function sumOfElements(array4:number[], isEven:boolean):number{
-//     return array4.reduce((sum,current) =>{
-//     })
-// }
+let array4 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+function sumOfElements(array4, isEven) {
+    return array4.reduce((sum, current) => {
+        if (isEven && current % 2 == 0) {
+            return sum + current;
+        }
+        else if (!isEven && current % 2 !== 0) {
+            return sum + current;
+        }
+        else {
+            return sum;
+        }
+    }, 0);
+}
+console.log('Sum of even', sumOfElements(array4, true));
+console.log('Sum of odd', sumOfElements(array4, false));
